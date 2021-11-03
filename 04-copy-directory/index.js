372be7678c380120f2b5601bc5795cdc65dcf3c3
l1irik-rbk <kir.baranow2010@gmail.com>
2021-11-03 20:10:04 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const path = require('path')
+const { copyFile, mkdir, readdir } = require('fs/promises')
 
+async function copyDir() {
+  try {
+    const pathFiles = path.join(__dirname, 'files')
+    const pathFilesCopy = path.join(__dirname, 'files-copy')
+    const files = await readdir(pathFiles, { withFileTypes: true })
+
+    mkdir(pathFilesCopy, { recursive: true }, () => {
+      console.log('Done!')
+    })
+
+    for (const file of files) {
+      if (file.isFile()) {
+        const oldFilePath = path.join(pathFiles, file.name)
+        const newFilePath = path.join(pathFilesCopy, file.name)
+        await copyFile(oldFilePath, newFilePath)
+      }
+    }
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+copyDir()
